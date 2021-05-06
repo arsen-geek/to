19ed05a304bd210d743f1a1a1d7cf8cc634d272e
Филипп <71774188+PhilBurub@users.noreply.github.com>
2021-05-06 13:34:08 +0300

    Update and rename tiktoxt.py to tiktoxt + choose_word.py

--- tiktoxt + choose_word.py
+++ tiktoxt + choose_word.py
@@ -2,11 +2,9 @@ import random
 from selenium import webdriver
 from TikTokApi import TikTokApi
 
-
 driver = webdriver.Chrome()
 api = TikTokApi.get_instance()
 
-    
 def tiktoxt(hashtag):
     try:
         dct = random.choice(api.by_hashtag(hashtag, count=5))
@@ -14,3 +12,11 @@ def tiktoxt(hashtag):
         return ''
     link = 'https://www.tiktok.com/@' + dct['author']['id'] + '/video/' + dct['video']['id']
     return link
+
+def choose_word(li_words):
+    n_list = li_words
+    random.shuffle(n_list)
+    for word in n_list:
+        link = tiktoxt(word)
+        if link:
+            return link
