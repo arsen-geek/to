cf8998bc1af4f805a7ab308ec5e2a0662392a2e3
katieTHEbest <56514599+katieTHEbest@users.noreply.github.com>
2021-05-13 19:30:01 +0300

    no thoughts head empty

--- tiktoxt.py
+++ tiktoxt.py
@@ -1,15 +1,21 @@
 import random
-from selenium import webdriver
 from TikTokApi import TikTokApi
 
-driver = webdriver.Chrome()
+from selenium import webdriver
+chrome_options = webdriver.ChromeOptions()
+chrome_options.add_argument("--headless")
+chrome_options.add_argument("--disable-gpu")
+browser = webdriver.Chrome(options=chrome_options)
+
 api = TikTokApi.get_instance()
+dummylink = 'https://vk.com/katakhma'
 
 def tiktoxt(hashtag):
     try:
         dct = random.choice(api.by_hashtag(hashtag, count=5))
-    except:
-        return ''
+    except AssertionError as error:
+        print(error)
+        return dummylink
     link = 'https://www.tiktok.com/@' + dct['author']['id'] + '/video/' + dct['video']['id']
     return link
 
@@ -20,4 +26,4 @@ def choose_word(li_words):
         link = tiktoxt(word)
         if link:
             return link
-    return ''
+    return tiktoxt('преисполниться')
