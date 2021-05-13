aa6e63a06c3c800023d6f3cff521d4052a23039e
katieTHEbest <56514599+katieTHEbest@users.noreply.github.com>
2021-05-13 19:34:25 +0300

    update2

--- tiktoxt.py
+++ tiktoxt.py
@@ -5,7 +5,7 @@ from selenium import webdriver
 chrome_options = webdriver.ChromeOptions()
 chrome_options.add_argument("--headless")
 chrome_options.add_argument("--disable-gpu")
-browser = webdriver.Chrome(options=chrome_options)
+browser = webdriver.Chrome(chrome_options=chrome_options)
 
 api = TikTokApi.get_instance()
 dummylink = 'https://vk.com/katakhma'
